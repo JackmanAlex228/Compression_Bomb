03f4a099cd4e60880551e7058880885d7fe73639
Alex Jackman <jackmanalex228@gmail.com>
2019-01-02 21:03:46 -0700

    Added some additional comments

--- compress.py
+++ compress.py
@@ -4,9 +4,12 @@ import os, sys, imghdr
 # scan all the photos in the same file as a script
 # save the compressed images into a new filder
 
-os.makedirs('compressed_images')
-path = os.path.dirname(os.path.realpath(__file__))
-dirs = os.listdir(path)
+os.makedirs('compressed_images')                        # create new folder for comrpessed images
+path = os.path.dirname(os.path.realpath(__file__))      # path = the current directory of the python script
+dirs = os.listdir(path)                                 # list of items in path
+
+# go through all jpeg and png files in the directory and then copy
+# the compressed version of these files into a new folder
 for item in dirs:
     # [Errno 13] Permission denied: 'compressed_images'
     # in some tests, it would compress some images but not all of them
